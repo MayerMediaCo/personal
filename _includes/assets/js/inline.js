fbd9f3edb7a26fb779dfe13ed60c7f9fdd9eb545
Danny Mayer <mayermediaco@gmail.com>
2019-06-16 21:38:48 -0500

    corrected responsive nav

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,10 +7,3 @@ if (window.netlifyIdentity) {
     }
   });
 }
-
-const mainNav = document.getElementById('js-menu');
-const navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function() {
-  mainNav.classList.toggle('active');
-});
