3c42d0ee8d7b70f161389e9540602f1050530423
Danny Mayer <mayermediaco@gmail.com>
2019-06-16 21:08:34 -0500

    Responsive Navbar

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,9 +1,16 @@
 if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
+  window.netlifyIdentity.on('init', user => {
     if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/admin/';
       });
     }
   });
 }
+
+const mainNav = document.getElementById('js-menu');
+const navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function() {
+  mainNav.classList.toggle('active');
+});
